0f54f4d2bcfa4d5bf7db990b8b5075c42723d891
JennerZhangYu <103395217+JennerZhangYu@users.noreply.github.com>
2022-04-19 16:32:47 +0800

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,9 @@
+function checkNum(){
+     var Num = parseInt(document.getElementById("id名称").value)
+     if(A,B > String || A,B < String || isNaN(A,B)){  
+     alert("Please enter the number in the input box");
+     document.getElementById('id名称').value = ""; }
+     var t=document.getElementById ("test").value 
+     var demoP=document.getElementById("demo"); 
+    demoP.innerHTML= Number(A) + Number(B)}
+
